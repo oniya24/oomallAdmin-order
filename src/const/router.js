3f8910402c94f586e5816271d5071b95b016663c
oniya24 <24320172203177@stu.xmu.edu.cn>
2021-04-19 22:55:47 +0800

    feat: 增加前缀

--- src/const/router.js
+++ src/const/router.js
@@ -1,4 +1,4 @@
 export const BASEURL =
   process.env.NODE_ENV == 'production'
-    ? 'http://47.114.166.8:8082'
-    : 'http://47.114.166.8:8082';
+    ? 'http://47.96.155.159:8082'
+    : 'http://47.96.155.159:8082';
